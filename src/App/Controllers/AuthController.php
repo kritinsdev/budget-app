1ffa8a7265c669d9e060710366a6826f8c8e2292
Kristaps Ritins <kritinsdev@gmail.com>
2023-12-27 23:45:55 +0200

    register functionality

--- src/App/Controllers/AuthController.php
+++ src/App/Controllers/AuthController.php
@@ -5,23 +5,33 @@ declare(strict_types=1);
 namespace App\Controllers;
 
 use Framework\TemplateEngine;
-use App\Services\ValidatorService;
+use App\Services\{
+    ValidatorService,
+    UserService
+};
 
 class AuthController
 {
     public function __construct(
         private TemplateEngine $view,
-        private ValidatorService $validatorService
+        private ValidatorService $validatorService,
+        private UserService $userService
     ) {
     }
 
     public function registerView()
     {
-        echo $this->view->render("/register.php");
+        echo $this->view->render("register.php");
     }
 
     public function register()
     {
         $this->validatorService->validateRegister($_POST);
+
+        $this->userService->isEmailTaken($_POST['email']);
+
+        $this->userService->create($_POST);
+
+        redirectTo('/');
     }
 }
---
+++ src/App/Services/UserService.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+use Framework\Database;
+use Framework\Exceptions\ValidationException;
+
+class UserService
+{
+    public function __construct(private Database $db)
+    {
+    }
+
+    public function isEmailTaken(string $email)
+    {
+        $emailCount = $this->db->query(
+            "SELECT COUNT(*) FROM users WHERE email = :email",
+            [
+                'email' => $email
+            ]
+        )->count();
+
+        if ($emailCount > 0) {
+            throw new ValidationException(['email' => 'Email taken']);
+        }
+    }
+
+    public function create(array $formData)
+    {
+        $password = password_hash($formData['password'], PASSWORD_BCRYPT, ['cost' => 12]);
+
+        $this->db->query(
+            "INSERT INTO users(email, password, age, country)
+            VALUES (:email, :password, :age, :country)",
+            [
+                'email' => $formData['email'],
+                'password' => $password,
+                'age' => $formData['age'],
+                'country' => $formData['country'],
+            ]
+        );
+    }
+}
